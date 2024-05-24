25cf3de7969a5878367721f4e59da02810f69ecd
EnocSalinas <wenocms@uamv.edu.ni>
2024-05-24 08:45:07 -0600

    4.cpp

---
+++ 4.cpp
@@ -0,0 +1,24 @@
+/*Escriba el programa que calcule la suma de los números impares
+mientras sean distintos de cero.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int num, suma = 0;
+    cout << "ingresa un numero:";
+    cin >> num;
+    while (num != 0)
+    {
+        if (num % 2 != 0)
+        {
+            suma += num;
+        }
+        cout << "ingresa un numero:";
+        cin >> num;
+    }
+    cout << "la suma de los numeros impares es: " << suma << endl;
+
+    return 0;
+}
