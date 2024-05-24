d3a94d839ee7a6c7d1bd375acc92bd3572e4832d
EnocSalinas <wenocms@uamv.edu.ni>
2024-05-24 09:00:36 -0600

    ejercicio 2 enoc

--- Ejercicio2Enoc/ejercicio2.cpp
+++ Ejercicio2Enoc/ejercicio2.cpp
@@ -1,22 +1,19 @@
-/*Crear un programa que solicite 15 numeros negativos y que los muestre en positivo y mostrar el resultado*/
-
-
 #include <iostream>
 using namespace std;
+
+void leeNumeros() {
+    int num, cont = 0;
+    cout << "Introduzca 15 numeros negativos: " << endl;
+    for(; cont < 15; cont++) {
+        cin >> num;
+        if(num < 0)
+            cout << "En forma positiva: " << num * -1 << endl;
+        else
+            cont--;
+    }
+}
+
 int main() {
-    int numbers[15];
-    
-    cout << "Ingrese 15 numeros negativos:" << endl;
-    
-    for (int i = 0; i < 15; i++) {
-        cin >> numbers[i];
-    }
-    
-    cout << "Numeros en positivo:" << endl;
-    
-    for (int i = 0; i < 15; i++) {
-        cout << abs(numbers[i]) << endl;
-    }
-    
+    leeNumeros();
     return 0;
 }
